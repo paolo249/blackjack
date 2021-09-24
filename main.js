7d2fa8148b712f39a08ed6f4daafa18f116f6574
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-09-24 01:08:54 -0700

    Revise ReadME

---
+++ main.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/
+// when it comes time to transfer the app's state to the DOM,
+// you can visualize the state anyway you want, e.g., a value of 1 is "rendered" 
+// with a certain image, etc.
+const cardLookup ={
+'0': null,
+'1': 1,
+
+}
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+//
+// init()
+// Transfer (Update) all state in the DOM
+// render()
+// Update all impacted state, then call render
+// handleBag() 
+//
