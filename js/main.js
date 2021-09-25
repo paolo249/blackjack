b65d781087098e00eb870f3356ceceb6b2ece710
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-09-25 16:10:08 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@
 // when it comes time to transfer the app's state to the DOM,
 // you can visualize the state anyway you want, e.g., a value of 1 is "rendered" 
 // with a certain image, etc.
-const cardLookup ={
+const cardLookup = {
 '0': null,
-'1': 1,
-
+'1': 1, '2': 2, '3': 3,
+'4': 4, '5': 5, '6': 6,
+'7': 7, '8': 8, '9': 9,
+'10': 10,
 }
 
 /*----- app's state (variables) -----*/
+let scoreVal, winner, betVal;
 
 /*----- cached element references -----*/
 
