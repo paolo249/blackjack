5c51c1c852d39db100fb838dd0f1993d70bffd3d
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-09-27 16:07:03 -0700

    major changes

--- js/main.js
+++ js/main.js
@@ -1,167 +1,100 @@
-// Notes:
-/*
-Troubleshoot Notes: 
-//** Main Problem - line 139: handleBag fxn, trying to update render
-// Mainly testing at this point line 128 to see if it comes out. 
-// Problem is all about getting suites from cardLookup function; 
-// Need to randomize the suites
-// using index in random generator in order to retrieve value in key-value
-//line 156: changing string into value ->(key-value) in order to read values in line 16  
-//line 112: Not going to use object, not necessary
-
-
-*/
-
-
-
 /*----- constants -----*/
-// when it comes time to transfer the app's state to the DOM,
-// you can visualize the state anyway you want, e.g., a value of 1 is "rendered" 
-// with a certain image, etc.
-const cardLookup = {
- 0 : {imgPath:  {b: 'img/backs/blue.svg',
-                 r: 'img/backs/red.svg'},
-      value: 0,          
-      },
- 1 : {imgPath:  {c: 'img/clubs/clubs-A.svg',
-                 d: 'img/diamonds/diamonds-A.svg',
-                 h: 'img/hearts/hearts-A.svg'},
-      value: 1,  
-      },
- 2 : {imgPath:  {c: 'img/clubs/clubs-r02.svg',
-                 d: 'img/diamonds/diamonds-r02.svg',
-                 h: 'img/hearts/hearts-r02.svg'}, 
-      value: 2,
-      },
- 3 : {imgPath:  {c: 'img/clubs/clubs-r03.svg',
-                 d: 'img/diamonds/diamonds-r03.svg',
-                 h: 'img/hearts/hearts-r03.svg'}, 
-      value: 3,
-      },
- 4 : {imgPath:  {c: 'img/clubs/clubs-r04.svg',
-                 d: 'img/diamonds/diamonds-r04.svg',
-                 h: 'img/hearts/hearts-r04.svg'}, 
-      value: 4,
-      },
- 5 : {imgPath:  {c: 'img/clubs/clubs-r05.svg',
-                 d: 'img/diamonds/diamonds-r05.svg',
-                 h: 'img/hearts/hearts-r05.svg'}, 
-      value: 5,
-      },
- 6 : {imgPath:  {c: 'img/clubs/clubs-r06.svg',
-                 d: 'img/diamonds/diamonds-r06.svg',
-                 h: 'img/hearts/hearts-r06.svg'}, 
-      value: 6,
-      },
- 7 : {imgPath:  {c: 'img/clubs/clubs-r07.svg',
-                 d: 'img/diamonds/diamonds-r07.svg',
-                 h: 'img/hearts/hearts-r08.svg'},
-      value: 7,
-      },
- 8 : {imgPath:  {c: 'img/clubs/clubs-r08.svg',
-                 d: 'img/diamonds/diamonds-r08.svg',
-                 h: 'img/hearts/hearts-r08.svg'}, 
-      value: 8,
-      },
- 9 : {imgPath:  {c: 'img/clubs/clubs-r09.svg',
-                 d: 'img/diamonds/diamonds-r09.svg',
-                 h: 'img/hearts/hearts-r09.svg'}, 
-      value: 9,
-      },
-10 : { k :     {imgPath:   {c: 'img/clubs/clubs-K.svg',
-                            d: 'img/diamonds/diamonds-K.svg',
-                            h: 'img/hearts/hearts-K.svg'},
-                value: 10,
-                   },                     
-       q :     {imgPath:   {c: 'img/clubs/clubs-Q.svg',
-                            d: 'img/diamonds/diamonds-Q.svg',
-                            h: 'img/hearts/hearts-Q.svg'},      
-                value: 10,
-                   },
-       j :     {imgPath:   {c: 'img/clubs/clubs-J.svg',
-                            d: 'img/diamonds/diamonds-J.svg',
-                            h: 'img/hearts/hearts-J.svg'},     
-                value: 10, 
-                   },
-       },
-    
-    };
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
-// // let sum; 
-// // sum = cardLookup[10].k.value + cardLookup[2].value;
-// // console.log(sum);
-            
+// Build a 'master' deck of 'card' objects used to create shuffled decks
+const masterDeck = buildMasterDeck();
+renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
 
 /*----- app's state (variables) -----*/
+let shuffledDeck;
 let scoreVal, winner, betVal;
 
-// /*----- cached element references -----*/
-// const pCardEl = document.getElementById('p-card');
-// const cCardEl = document.getElementById('c-card');
-// const cCardEl2 = document.getElementById('c-card2');
+/*----- cached element references -----*/
+const shuffledContainer = document.getElementById('shuffled-deck-container');
+const pCardEl = document.getElementById('p-card');
+const cCardEl = document.getElementById('c-card');
 
 /*----- event listeners -----*/
-//document.querySelector('main').addEventListener('click', handleBag);
- 
+document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
+
 /*----- functions -----*/
 init();
-function init(){
-     //Chip bet    
-     // Cards
-  cardResults = {
-        p:  10,
-        c:  1,
-       c2:  0
-       };
-   cardSuit = {
-         c : 'c',
-         d : 'd',
-         h : 'h'
-       };
-   //winner = 't';
-      //render();   
-      
+function init() {
+
+cardResults = {
+     p:  'r',
+     c:  'r',
+    };
+
+
+
+
 }
 
-// // Transfer (Update) all state in the DOM
-render()
+function getNewShuffledDeck() {
+  // Create a copy of the masterDeck (leave masterDeck untouched!)
+  const tempDeck = [...masterDeck]; //accesses master deck length  (basically runs through the whole 2d array)
+  //troubleshoot: use some loop that just acceses two cards
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    // Get a random index for a card still in the tempDeck
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
+  return newShuffledDeck;
+}
+
 function render() { 
-cCardEl.src = cardLookup[cardResults.c].imgPath.c;
-//cCardEl.src = getRandomCard().randInd();
+          cCardEl.src = cardLookup[cardResults.c].imgPath.c;
+          //cCardEl.style.borderColor =  'black';
 
-cCardEl.style.borderColor =  'black';
-cCardEl2.src = cardLookup[cardResults.c2].imgPath.b;
-cCardEl2.style.borderColor = 'black';
+          //cCardEl2.src = cardLookup[cardResults.c2].imgPath.b;
+          //cCardEl2.style.borderColor = 'black';
 
-pCardEl.src = cardLookup[cardResults.p].q.imgPath.c;
-pCardEl.style.borderColor =  'black';
+          pCardEl.src = cardLookup[cardResults.p].q.imgPath.c;
+          //pCardEl.style.borderColor =  'black';
+          }
+          
+
+
+function renderNewShuffledDeck() {
+  // Create a copy of the masterDeck (leave masterDeck untouched!)
+  shuffledDeck = getNewShuffledDeck();
+  renderDeckInContainer(shuffledDeck, shuffledContainer);
 }
 
-
-// // Update all impacted state, then call render
-// // handleBag() 
-function handleBag(evt){
-    cardResults.p = evt.target.textContent.toLowerCase();
-    cardResults.c = getRandomCard();
-    cardLookup[cardResults.c];
-    render();
+function renderDeckInContainer(deck, container) {
+  container.innerHTML = '';
+  // Let's build the cards as a string of HTML
+  let cardsHtml = '';
+  deck.forEach(function(card) {
+    cardsHtml += `<div class="card ${card.face}"></div>`;
+  });
+  // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+  // const cardsHtml = deck.reduce(function(html, card) {
+  //   return html + `<div class="card ${card.face}"></div>`;
+  // }, '');
+  container.innerHTML = cardsHtml;
 }
 
-function getRandomCard(){
+function buildMasterDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        //face: `${suit}${rank}`,
+        face: `${suit}${rank}`,
+        // Setting the 'value' property for game of blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
 
-    const idx = Math.floor(Math.random()*10);
-     let randCard = cardLookup[idx].imgPath;
-    //let randCard = cardLookup[idx].value;
-    //console.log(idx);
-   // consols.log(cardLookup[idx].value);
-    return randCard;
+      });
+    });
+  });
+  return deck;
 }
 
- //Troubleshoot Notes: changing string into value ->(key-value)  
- // Testing lines 162-165 in order to retrieve random suite
-//    const randArr = ['c','d','h'];
-//    const idx = Math.floor(Math.random()*2); 
-//    const randSuit = randArr[idx];
-//    console.log(randSuit);
-   
-
+renderNewShuffledDeck();
